d4e8eba19b3dec9d32451822d36b03e80053a307
PELLICER PALACIOS ANALIA ANAHI <analiapellicer86@gmail.com>
2023-09-15 17:15:11 -0300

    se trabajó nuevamente sobre los filtros en base a las sugerencias recibidas

--- frontend/scripts/filtro.js
+++ frontend/scripts/filtro.js
@@ -1,18 +1,18 @@
-const categorias = ["Novedades", "Infantiles", "Ficcion", "Filosofia y Religíon"];
+const categorias = ["Romance", "Infantiles", "Ficcion", "Filosofia y Religíon"];
 
 function createCategoryButton(categoria, index) {
   const button = document.createElement("button");
   button.textContent = categoria;
-  button.id = `boton-novedades-${index}`; 
-  button.id = `boton-infantiles-${index}`; 
-  button.id = `boton-ficcion-${index}`; 
-  button.id = `boton-filosofia-${index}`; 
+  button.id = "boton-romance"; 
+  button.id = "boton-infantiles"; 
+  button.id = "boton-ficcion"; 
+  button.id = "boton-filosofia"; 
   
   button.addEventListener("click", function () {
 
       // Al hacer clic en el botón, redirige a la página de productos y guarda la categoría seleccionada en el localStorage
       localStorage.setItem("categoria", categoria);
-      window.location.href = "Productos.html";
+      window.location.href("./template/productos/html");
     });
   return button;
 }
