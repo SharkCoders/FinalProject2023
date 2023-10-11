5fca938625baac050c0ae90ac1703573cbead606
Ivette <nobiltaivette@gmail.com>
2023-10-11 11:58:09 -0300

    corrección relative path

--- frontend/scripts/scriptIndex.js
+++ frontend/scripts/scriptIndex.js
@@ -8,7 +8,7 @@ let fraseActualIndex = 0;
 
 // Función para cargar frases desde un archivo json
 function cargarFrasesDesdeArchivo() {
-    fetch('/frases.json') // Reemplaza 'frases.txt' con la ruta correcta a tu archivo json
+    fetch('../../frases.json') // Reemplaza 'frases.txt' con la ruta correcta a tu archivo json
         .then(response => response.json())
         .then(data => {
             // Extraer las frases de la propiedad "frase" en el objeto JSON
