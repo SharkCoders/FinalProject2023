630e81d1ab854d538826f47028ca5802b775743c
PELLICER PALACIOS ANALIA ANAHI <analiapellicer86@gmail.com>
2023-10-18 13:39:10 -0300

    incorporación de  css y arreglos en filter-books

--- frontend/scripts/filter-books.js
+++ frontend/scripts/filter-books.js
@@ -1,55 +1,67 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Variables
-    const booksContainer = document.getElementById('cont-de-cat');
-    const categoryButtons = document.getElementById('category-buttons');
-  
-    // Cargar los datos del archivo JSON
-    fetch('books.json')
-      .then(response => response.json())
-      .then(data => {
-        const books = data;
-  
-        // Función para mostrar libros
-        function displayBooks(books) {
-          booksContainer.innerHTML = ''; // Vacía el contenedor de libros
-  
-          books.forEach(book => {
-            // Crea un elemento div para mostrar el libro
-            const bookDiv = document.createElement('div');
-            bookDiv.classList.add('book');
-            bookDiv.innerHTML = `
-              <h2>${book.titulo}</h2>
-              <p>Autor: ${book.autor}</p>
-              <p>Categoría: ${book.categoria}</p>
-              <p>Descripción: ${book.descripcion}</p>
-              <p>Precio: $${book.precio}</p>
-            `;
-  
-            booksContainer.appendChild(bookDiv);
-          });
-        }
-  
-        // Mostrar todos los libros al cargar la página
-        displayBooks(books);
-  
+  // Variables
+  const categoryButtons = document.getElementById('category-buttons'); 
 
-        // Contenedor para los botones
-const buttonContainer = document.createElement('div');
-buttonContainer.classList.add('button-container');
-document.body.appendChild(buttonContainer);
+  // Cargar los datos desde el archivo JSON local
+  const jsonUrl = 'books.json';
+  console.log('Solicitando JSON desde:', jsonUrl);
+  fetch(jsonUrl)
+    .then(response => response.json())
+    .then(data => {
+      allBooks = data;
 
-// Crear botones de categoría
-const categories = [...new Set(books.map(book => book.categoria))];
+      // Crear botones de categoría
+      const categories = [...new Set(allBooks.map(book => book.categoria))]; 
+      categories.forEach(category => {
+        const button = document.createElement('button');
+        button.textContent = category;
+        button.classList.add('category-button');
+        button.addEventListener('click', () => {
+          const filteredBooks = allBooks.filter(book => book.categoria === category);
+          showBooks(filteredBooks);
+        });
+        categoryButtons.appendChild(button); // Agregar el botón al div existente
+      });
+    })
+    .catch(error => console.error(error));
 
-categories.forEach(category => {
-  const button = document.createElement('button');
-  button.textContent = category;
-  button.classList.add('category-button');
-  button.addEventListener('click', () => {
-    const filteredBooks = books.filter(book => book.categoria === category);
-    displayBooks(filteredBooks);
-  });
-  buttonContainer.appendChild(button);
-})
-  });
-})
+  // Función para mostrar libros queda igual si mal no recuerdo
+  function showBooks(books) {
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = ''; // Limpiar resultados anteriores
+    books.forEach(book => {
+      const card = createCard(book);
+      cardContainer.appendChild(card);
+    });
+  }
+
+  // Función para crear una tarjeta de libro esta la modifiqué, porque con tu código declarado, creaba una tarjeta nueva sobre la anterior
+  function createCard(bookInfo) {
+    const card = document.createElement('div');
+    card.classList.add('product-card');
+
+    const cardContent = `
+      <div class="card text-bg-secondary mb-1" style="max-width: 700px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${bookInfo.portada}" alt="${bookInfo.titulo}" class="card-img-top">
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <p id="title"><br>Título: ${bookInfo.titulo}</p>
+              <p>Autor: ${bookInfo.autor || "Desconocido"}</p>
+              <p>Categoría: ${bookInfo.categoria || "Desconocida"}</p>
+              <p>Precio: $ ${bookInfo.precio}</p>
+              <p>Stock: ${bookInfo.stock}</p>
+              <a href="../../carrito.html" class="btn btn-dark">Comprar</a>
+              <a href="../../descripcion.html?title=${encodeURIComponent(bookInfo.titulo)}" class="btn btn-light">Resumen</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    card.innerHTML = cardContent;
+    return card;
+  }
+});
