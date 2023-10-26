b49f5adbbb25cee05dee442ef678f1e70995553c
Leopladom <114880850+Leopladom@users.noreply.github.com>
2023-10-25 21:01:11 -0300

    .

--- frontend/scripts/scriptDescripcion.js
+++ frontend/scripts/scriptDescripcion.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const bookTitle = urlParams.get("title");
   
     // Encontrar el libro correspondiente en el array de libros
-    const jsonUrl = "books.json";
+    const jsonUrl = "/books.jsonbooks.json";
     fetch(jsonUrl)
       .then((response) => {
         return response.json()})
