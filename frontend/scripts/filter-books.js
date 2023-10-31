85ad100e64198b6657710db7742043c2a1523fb4
Ivette <nobiltaivette@gmail.com>
2023-10-31 12:21:05 -0300

    pruebas para deploy GHPages

--- frontend/scripts/filter-books.js
+++ frontend/scripts/filter-books.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const filterSelect = document.getElementById("filter-select");
 
   // Cargar los datos desde el archivo JSON local
-  const jsonUrl = "../../books.json";
+  const jsonUrl = "books.json";
   fetch(jsonUrl)
     .then(response => response.json())
     .then(data => {
