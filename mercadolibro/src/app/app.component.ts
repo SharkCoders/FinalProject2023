e1a6aa3ef415e4c0d4a7b09a7aaa12b2b497e270
Leopladom <114880850+Leopladom@users.noreply.github.com>
2024-05-01 02:20:30 -0300

    nav

--- mercadolibro/src/app/app.component.ts
+++ mercadolibro/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent],
+  imports: [RouterOutlet, LandingComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
